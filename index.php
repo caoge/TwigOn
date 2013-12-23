50401ba9fbc99384bea4eda37bcd97fa5e05e2d2
caoge <caogemail@163.com>
2013-12-23 17:31:01 +0800

    edit

--- index.php
+++ index.php
@@ -18,13 +18,23 @@ class Project_Article_TokenParser extends Twig_TokenParser {
         
         $service = $stream->expect(Twig_Token::NAME_TYPE, 'service')->getValue();
         $b = $stream->expect(Twig_Token::OPERATOR_TYPE, '=')->getValue();
-        $service1 = $parser->getExpressionParser()->parseExpression();
+        $service_name = $parser->getExpressionParser()->parseExpression();
 
 
         $param = $stream->expect(Twig_Token::NAME_TYPE)->getValue();
+        
         $stream->expect(Twig_Token::OPERATOR_TYPE, '=');
-        $param_value = $parser->getExpressionParser()->parseExpression();
+        //$test = $stream->expect(Twig_Token::STRING_TYPE);
 
+        $param_value = $parser->getExpressionParser()->parseExpression();
+        // if ($param_token = $stream->nextIf(Twig_Token::STRING_TYPE)) {
+        //     $param_value = $param_token->getValue();
+        // }
+
+        //var_dump($param_value);
+        // $param_value = $stream->nextIf(Twig_Token::STRING_TYPE)->getValue();
+       // var_dump(json_encode(array('a' => 333, 'b' => 'sfsf')));//[333,"sfsf"]
+        //var_dump(json_decode('{"user_id":"3", "user_name":"caoge"}'));exit;
 
         // while (!$token->test(Twig_Token::BLOCK_END_TYPE, 'endarticle')) {
         //     var_dump(2);
@@ -43,7 +53,7 @@ class Project_Article_TokenParser extends Twig_TokenParser {
         $body = $parser->subparse(array($this, 'decideArticleEnd'), true);
         $stream->expect(Twig_Token::BLOCK_END_TYPE);
 
-
+        
         $keyTarget = new Twig_Node_Expression_AssignName('_key', $lineno);
         $valueTarget = new Twig_Node_Expression_AssignName('_field', $lineno);
 
@@ -99,9 +109,9 @@ class Project_Clist_TokenParser extends Twig_TokenParser {
 // 转成PHP代码
 class Project_Article_Node extends Twig_Node {
 	public function __construct(Twig_Node_Expression_AssignName $keyTarget, Twig_Node_Expression_AssignName $valueTarget, Twig_NodeInterface $body, $param, Twig_Node_Expression $param_value, $lineno, $tag = null) {
-        $body = new Twig_Node(array($body, $this->loop = new Twig_Node_ForLoop($lineno, $tag)));
-
-        parent::__construct(array('key_target' => $keyTarget, 'value_target' => $valueTarget, 'body' => $body), array('with_loop' => true), $lineno, $tag);
+        $body = new Twig_Node(array($body, $this->loop = new Twig_Node_ForLoop($lineno, $tag)));//Twig_Node_Expression param_value
+        //var_dump($param);
+        parent::__construct(array('key_target' => $keyTarget, 'value_target' => $valueTarget, 'body' => $body, 'param_value' => $param_value), array('with_loop' => true, 'param' => $param), $lineno, $tag);
 
 		//parent::__construct(array('param_value' => $param_value), array('param' => $param), $lineno, $tag);
 	}
@@ -111,18 +121,29 @@ class Project_Article_Node extends Twig_Node {
             array('id' => 1, 'name' => 'a'),
             array('id' => 2, 'name' => 'b')
         );
-        $a = json_encode(array('caoge', 'bbb'));
+        $a = json_encode(array('t' => array('caoge', 'bbb'), 'b' => array('ccc', 'ddd')));
         //var_dump($a);
         //var_dump(json_decode('["caoge","bbb"]'));
         $compiler
             ->addDebugInfo($this)
             ->write("\$context['_seq'] = array(11);")
-            ->write("\$context['json'] = json_decode('".$a."');")
+            ->write("\$context['json'] = json_decode('".$a."', true);")
+            ->raw(";\n")
+        ;
+        //var_dump($aaa);exit;
+        //getKeyValuePairs
+        //$aaa = $this->getNode('param_value')->getKeyValuePairs();
+        //var_dump($aaa);exit;
+        //var_dump($aaa[0]['key']);exit;
+
+        $compiler
+            ->addDebugInfo($this)
+            ->write('$context[\''.$this->getAttribute('param').'\'] = ')
+            ->subcompile($this->getNode('param_value'))
             ->raw(";\n")
         ;
 
-
-        //var_dump($this->getNode('body'));exit;
+        
         $compiler
             ->write("foreach (\$context['json'] as ")
             ->subcompile($this->getNode('key_target'))
@@ -134,16 +155,9 @@ class Project_Article_Node extends Twig_Node {
             ->outdent()
             ->write("}\n")
         ;
-        // $compiler
-        //     ->addDebugInfo($this)
-        //     ->write('$context[\''.$this->getAttribute('param').'\'] = ')
-        //     ->subcompile($this->getNode('param_value'))
-        //     ->raw(";\n")
-        // ;
+        error_log($compiler->getSource(), 3, "log.php");
     }
 }
-
-
 
 $twig->addTokenParser(new Project_Article_TokenParser());
 //$twig->addTokenParser(new Project_Set_TokenParser());
